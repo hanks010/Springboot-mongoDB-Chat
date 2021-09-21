38446087fd783ca5acf133a0189c67b1cc432357
hanks010 <hanks010@naver.com>
2021-09-21 15:55:35 +0900

    Feat: 추가 자바스크립트

--- chat_front/js/chat.js
+++ chat_front/js/chat.js
@@ -1,1 +1,75 @@
+const eventSource = new EventSource(
+  'http://localhost:8080/sender/ssar/receiver/cos'
+);
 
+eventSource.onmessage = (event) => {
+  console.log(1, event);
+  const data = JSON.parse(event.data);
+  console.log(2, data);
+  InitMessage(data);
+};
+
+function getSendMsgBox(msg, time) {
+  return `<div class="sent_msg">
+  <p>${msg}</p>
+  <span class="time_date"> ${time} </span>
+</div>`;
+}
+
+function InitMessage(data) {
+  let chatBox = document.querySelector('#chat-box'); //채팅 박스 선택
+  let msgInput = document.querySelector('#chat-outgoing-msg'); //입력창 선택
+
+  //alert(msgInput.value);
+  let chatOutgoingBox = document.createElement('div'); //새로운 div 영역 추가
+  chatOutgoingBox.className = 'outgoing_msg'; //클래스명 추가,새로운 div에 디자인 적용됨
+  let month = data.createdAt.substring(5, 7);
+  let date = data.createdAt.substring(8, 10);
+  let hour = data.createdAt.substring(11, 13);
+  let minute = data.createdAt.substring(14, 16);
+  let time = hour + ':' + minute + ' | ' + month + '/' + date;
+  chatOutgoingBox.innerHTML = getSendMsgBox(data.msg, time); //입력창에 들어온 값을 div 영역 안에 추가함
+  chatBox.append(chatOutgoingBox); //채팅 박스에 내용 추가
+  msgInput.value = ''; //입력창 비워줌
+}
+
+function addMessage() {
+  let chatBox = document.querySelector('#chat-box'); //채팅 박스 선택
+  let msgInput = document.querySelector('#chat-outgoing-msg'); //입력창 선택
+
+  //alert(msgInput.value);
+  let chatOutgoingBox = document.createElement('div'); //새로운 div 영역 추가
+  chatOutgoingBox.className = 'outgoing_msg'; //클래스명 추가,새로운 div에 디자인 적용됨
+
+  let date = new Date(); //현재 날짜 및 시간
+  let Minute = date.getMinutes();
+  if (Minute < 10) {
+    Minute = '0' + date.getMinutes();
+  }
+  let now =
+    date.getHours() +
+    ':' +
+    Minute +
+    ' | ' +
+    (date.getMonth() + 1) +
+    '/' +
+    date.getDate();
+
+  chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value, now); //입력창에 들어온 값을 div 영역 안에 추가함
+  chatBox.append(chatOutgoingBox); //채팅 박스에 내용 추가
+  msgInput.value = ''; //입력창 비워줌
+}
+
+document
+  .querySelector('#chat-outgoing-button') //전송 버튼 클릭 리스너 추가
+  .addEventListener('click', () => {
+    addMessage();
+  });
+
+document
+  .querySelector('#chat-outgoing-msg')
+  .addEventListener('keydown', (e) => {
+    if (e.keyCode == 13) {
+      addMessage();
+    }
+  });
--- chatapp/src/main/java/com/cos/chatapp/ChatController.java
+++ chatapp/src/main/java/com/cos/chatapp/ChatController.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,12 +22,14 @@ public class ChatController {
 	private ChatRepository chatRepository;
 	
 	//데이터를 받아오는 컨트롤러
+	@CrossOrigin
 	@GetMapping(value ="/sender/{sender}/receiver/{receiver}",produces=MediaType.TEXT_EVENT_STREAM_VALUE)//SSE 프로토콜을 의미
 	public Flux<Chat> getMsg(@PathVariable String sender,@PathVariable  String receiver) {
 		return chatRepository.mFindBySender(sender, receiver)
 		.subscribeOn(Schedulers.boundedElastic());
 	}
 	//데이터를 입력하는 컨트롤러
+	@CrossOrigin
 	@PostMapping("/chat")
 	public Mono<Chat> setMsg(@RequestBody Chat chat){
 		//Mono는 데이터를 한 번만 리턴한다는 의미, 리턴타입을 void로 설정해도 된다.
